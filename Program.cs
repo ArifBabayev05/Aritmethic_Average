dee4dc0b256828fed0f33d66002f84b38bdf6899
ArifBabayev05 <101328505+ArifBabayev05@users.noreply.github.com>
2022-03-17 01:24:54 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AritmethicCenter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double average;
+            double[] number = {1,62,3,4,35,6,73};
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum=sum+number[i];
+            }
+            average = sum/number.Length;
+            Console.WriteLine("This Is Average Of Numbers: "+average);
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("This Is Sum OF Numbers: "+sum); 
+           
+
+
+
+
+        }
+    }
+}
